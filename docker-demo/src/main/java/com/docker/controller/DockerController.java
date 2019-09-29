1b1c5b1c8d196d723073d6dfd12ccdd3128a679b
Vijay Peddy <vijaypeddy@VijayPeddysMac.cable.rcn.com>
2019-09-29 11:29:47 -0400

    Docker-demo initial commit

---
+++ docker-demo/src/main/java/com/docker/controller/DockerController.java
@@ -0,0 +1,20 @@
+package com.docker.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+	
+	private static Logger logger = LoggerFactory.getLogger(DockerController.class);
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String showLoginPage() {
+		logger.info("Docker-demo test Success!!");
+		return "Docker-demo test Success!!";
+	}
+	
+}
